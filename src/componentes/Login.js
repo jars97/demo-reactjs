ba19b63daf6faaa80f9e0531eda728f72777a7c5
joserodriguez <jars97@gmail.com>
2021-10-03 22:51:50 -0400

    wip

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -11,11 +11,28 @@ import IconButton from '@material-ui/core/IconButton';
 
 export class Login extends Component{
   state = {
-    showPassword: false
+    showPassword: false,
+    disableButton: false,
+    email:"",
+    password:""
   }
   
   _handleClickShowPassword = (e) => {
     this.setState({ showPassword: !this.state.showPassword })
+  }
+
+  _onChangeEmail = (e)=>{
+    this.setState({ email: e.target.value })
+    var longitud = e.target.value.length
+    var long_pass = this.state.password.length
+    this.setState({ disableButton: longitud>0 && long_pass>0 ? false : true })  
+  }
+
+  _onChangePassword = (e)=>{
+    this.setState({ password: e.target.value })
+    var longitud = e.target.value.length
+    var long_email = this.state.email.length
+    this.setState({ disableButton: longitud>0 && long_email>0 ? false : true })  
   }
 
 
@@ -32,16 +49,19 @@ export class Login extends Component{
               required
               id="outlined-required"
               label="Correo electrónico"
+              value={this.state.email}
               variant="outlined"
+              onChange={this._onChangeEmail}
           />
 
           <TextField
               required
               id="outlined-required"
-              
               variant="outlined"
               label="Contraseña"
+              value={this.state.password}
               type={this.state.showPassword ? 'text' : 'password'}
+              onChange={this._onChangePassword}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="start">
@@ -69,9 +89,10 @@ export class Login extends Component{
           <Button className="recuperar-clave" color="primary">Recuperar clave</Button>
         </div>
         <br></br>
-        <Button variant="contained" color="primary">
-          Iniciar sesion
-        </Button>
+        {this.state.disableButton ? <Button variant="contained" color="primary" disabled>Iniciar sesion </Button>: <Button variant="contained" color="primary" >Iniciar sesion</Button>}
+        
+          
+        
         <br></br>
         <span className="nueva-cuenta">¿Eres un nuevo usuario? <a href="./home" className="solicita"> Solicita una cuenta</a></span>
     </div>
