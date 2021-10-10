775ff180a20991b6e2111d36c428be5437005291
joserodriguez <jars97@gmail.com>
2021-10-10 08:49:11 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -25,9 +25,9 @@ const theme = createTheme({
 
 ReactDOM.render(
   <MuiThemeProvider theme = { theme }>
-    <React.StrictMode>
+   
       <App />
-    </React.StrictMode>,
+    
   </MuiThemeProvider>, 
   
   document.getElementById('root')
