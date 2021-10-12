6487290d85b2a183d2d3a37525e38b027e01de78
joserodriguez <jars97@gmail.com>
2021-10-11 21:40:53 -0400

    code clean up

--- src/bancos/BancosList.js
+++ src/bancos/BancosList.js
@@ -7,21 +7,17 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TablePagination from '@material-ui/core/TablePagination';  
 import TableRow from '@material-ui/core/TableRow';     
 import { useState, useEffect } from 'react' 
-import { withStyles,makeStyles, useTheme } from '@material-ui/core/styles'; 
+import { withStyles,makeStyles } from '@material-ui/core/styles'; 
 import BancosService from './BancosService';
 import BancosAdd from './BancosAdd';
 import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
-import PropTypes from 'prop-types';
 import TableHeader from '../componentes/TableHeader';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import RemoveDialog from '../componentes/RemoveDialog';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
+import CustomTablePaginationActions from '../componentes/TablePaginationActions';  
 
 const useStyles1 = makeStyles((theme) => ({
     root: {
@@ -42,66 +38,7 @@ const useStyles1 = makeStyles((theme) => ({
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
-
-function TablePaginationActions(props) {
-    const classes = useStyles1();
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onPageChange } = props;
-  
-    const handleFirstPageButtonClick = (event) => {
-      onPageChange(event, 0);
-    };
-  
-    const handleBackButtonClick = (event) => {
-      onPageChange(event, page - 1);
-    };
-  
-    const handleNextButtonClick = (event) => {
-      onPageChange(event, page + 1);
-    };
-  
-    const handleLastPageButtonClick = (event) => {
-      onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-  
-    return (
-      <div className={classes.footer}>
-        <IconButton
-          onClick={handleFirstPageButtonClick}
-          disabled={page === 0}
-          aria-label="first page"
-        >
-          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-        </IconButton>
-        <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-        </IconButton>
-        <IconButton
-          onClick={handleNextButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="next page"
-        >
-          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-        </IconButton>
-        <IconButton
-          onClick={handleLastPageButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="last page"
-        >
-          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-        </IconButton>
-      </div>
-    );
-  }
-  
-  TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onPageChange: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-  };
-
+} 
 
 const StyledTableRow = withStyles((theme) => ({
     root: {
@@ -209,7 +146,7 @@ const BancosList = () => {
                 page={page}
                 onPageChange={handleChangePage} 
                 onRowsPerPageChange={handleChangeRowsPerPage} 
-                ActionsComponent={TablePaginationActions}
+                ActionsComponent={CustomTablePaginationActions}
             />
             <RemoveDialog open={open} setOpen={setOpen} removeRecord={removeRecord}></RemoveDialog>
           </Paper>
